f9a3894c9b2ec6f7f247b3e37e1ebbf7c0a93e56
phước em <phuocanh554@gmail.com>
2025-06-02 17:24:47 +0700

    Improves table management and order display
    Refactors the table selection form to dynamically display tables
    based on their status (free/occupied) and type (dine-in/takeaway).
    Adds tooltips to table buttons for status information.
    Updates the order form with product categories and a search function,
    allowing for easier product selection.
    Includes visual updates such as table status icons and product images.
    Updates the database connection string.

--- IPOS/Form_Table.Designer.cs
+++ IPOS/Form_Table.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class Form_Table
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,29 +13,22 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_Table));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.button13 = new System.Windows.Forms.Button();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.button1 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
-            this.tabPage2.SuspendLayout();
             this.flowLayoutPanel1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -50,9 +36,8 @@
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1239, 30);
-            this.menuStrip1.TabIndex = 12;
-            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1024, 24);
+            this.menuStrip1.TabIndex = 0;
             // 
             // contextMenuStrip1
             // 
@@ -62,97 +47,100 @@
             // 
             // button13
             // 
-            this.button13.Location = new System.Drawing.Point(945, 604);
+            this.button13.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button13.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.button13.Location = new System.Drawing.Point(0, 718);
             this.button13.Name = "button13";
-            this.button13.Size = new System.Drawing.Size(215, 45);
-            this.button13.TabIndex = 14;
-            this.button13.Text = "Shift Management";
+            this.button13.Size = new System.Drawing.Size(1024, 50);
+            this.button13.TabIndex = 2;
+            this.button13.Text = "Quản lý ca làm việc";
             this.button13.UseVisualStyleBackColor = true;
             // 
             // tabControl1
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Cursor = System.Windows.Forms.Cursors.Default;
-            this.tabControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
-            this.tabControl1.Location = new System.Drawing.Point(0, 12);
-            this.tabControl1.Multiline = true;
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.tabControl1.Location = new System.Drawing.Point(0, 24);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1227, 586);
-            this.tabControl1.TabIndex = 15;
+            this.tabControl1.Size = new System.Drawing.Size(1024, 694);
+            this.tabControl1.TabIndex = 1;
             // 
             // tabPage1
             // 
             this.tabPage1.Controls.Add(this.flowLayoutPanel1);
-            this.tabPage1.Location = new System.Drawing.Point(4, 47);
+            this.tabPage1.Location = new System.Drawing.Point(4, 34);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(1219, 535);
+            this.tabPage1.Size = new System.Drawing.Size(1016, 656);
             this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "Tại Chỗ";
+            this.tabPage1.Text = "Tại chỗ";
             this.tabPage1.UseVisualStyleBackColor = true;
-            // 
-            // tabPage2
-            // 
-            this.tabPage2.Controls.Add(this.flowLayoutPanel2);
-            this.tabPage2.Location = new System.Drawing.Point(4, 47);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1219, 535);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Take Away";
-            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // flowLayoutPanel1
             // 
+            this.flowLayoutPanel1.AutoScroll = true;
             this.flowLayoutPanel1.Controls.Add(this.button1);
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(6, 6);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(1217, 526);
+            this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(10);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1016, 656);
             this.flowLayoutPanel1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
-            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.button1.Location = new System.Drawing.Point(13, 13);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(238, 148);
-            this.button1.TabIndex = 13;
-            this.button1.Text = "1";
+            this.button1.Size = new System.Drawing.Size(200, 120);
+            this.button1.TabIndex = 0;
             this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.flowLayoutPanel2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 34);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Size = new System.Drawing.Size(1016, 656);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Mang về";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
             // flowLayoutPanel2
             // 
-            this.flowLayoutPanel2.Location = new System.Drawing.Point(21, 20);
+            this.flowLayoutPanel2.AutoScroll = true;
+            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(0, 0);
             this.flowLayoutPanel2.Name = "flowLayoutPanel2";
-            this.flowLayoutPanel2.Size = new System.Drawing.Size(1192, 495);
+            this.flowLayoutPanel2.Padding = new System.Windows.Forms.Padding(10);
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(1016, 656);
             this.flowLayoutPanel2.TabIndex = 0;
             // 
             // Form_Table
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1239, 651);
+            this.ClientSize = new System.Drawing.Size(1024, 768);
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.button13);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form_Table";
-            this.Text = "Form_Table";
+            this.Text = "Quản lý bàn";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Form_Table_Load);
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
-            this.tabPage2.ResumeLayout(false);
             this.flowLayoutPanel1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
-        #endregion
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.Button button13;
--- IPOS/Form_Table.cs
+++ IPOS/Form_Table.cs
@@ -2,12 +2,7 @@
 using IPOS.DB_Access;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace IPOS
@@ -30,57 +25,46 @@ namespace IPOS
             List<Table_Details> danhSachBan = tda.GetAllTables();
 
             flowLayoutPanel1.Controls.Clear();
+            flowLayoutPanel2.Controls.Clear();
+
+            ToolTip tooltip = new ToolTip();
 
             foreach (var ban in danhSachBan)
             {
-                Button btn = new Button();
+                Button btn = new Button
+                {
+                    Size = button1.Size,
+                    Font = new Font("Segoe UI", 10, FontStyle.Bold),
+                    Margin = button1.Margin,
+                    ForeColor = ban.Status == "free" ? Color.Green : Color.Red,
+                    TextAlign = ContentAlignment.BottomCenter,
+                    FlatStyle = button1.FlatStyle,
+                    Text = ban.nameTable,
+                    Tag = ban,
+                    Name = $"btnBan{ban.ID}",
+                    ImageAlign = ContentAlignment.TopCenter
+                };
 
-              
-                btn.Size = button1.Size;
-                btn.Font = button1.Font;
-                btn.Margin = button1.Margin;
-                btn.Image = button1.Image;
-                btn.TextAlign = ContentAlignment.BottomCenter;
-                btn.ForeColor = button1.ForeColor;
-
-                btn.Font = new Font("Segoe UI", 10, FontStyle.Bold); // Cỡ, kiểu chữ
-                btn.ForeColor = ban.Status == "free" ? Color.Green : Color.Red;
-                btn.TextAlign = ContentAlignment.BottomCenter; // Căn chữ
-
-
-                btn.FlatStyle = button1.FlatStyle;
-
-             
-                btn.Text = ban.nameTable;
-                btn.Tag = ban;
-                btn.Name = $"btnBan{ban.ID}";
-
-               
+                btn.Image = ban.Status == "free" ? Properties.Resources.Table_Free : Properties.Resources.Table_Occupied;
+                tooltip.SetToolTip(btn, $"Bàn: {ban.nameTable}\nTrạng thái: {ban.Status}");
                 btn.Click += Btn_Click;
 
-           
                 if (ban.IsTakeAway)
-                {
                     flowLayoutPanel2.Controls.Add(btn);
-                }
                 else
-                {
                     flowLayoutPanel1.Controls.Add(btn);
-                }
             }
 
             button1.Visible = false;
         }
 
-
         private void Btn_Click(object sender, EventArgs e)
         {
-            Button btn = sender as Button;
-            Table_Details ban = btn.Tag as Table_Details;
-
-            Order ord = new Order();
-            ord.Show();
-
+            if (sender is Button btn && btn.Tag is Table_Details ban)
+            {
+                Order ord = new Order();
+                ord.Show();
+            }
         }
     }
 }
